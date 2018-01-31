1398ffdf968775fd1883698df9133b6d042571c6
arfost <clement.chevalier@cirruseo.com>
2018-02-01 00:04:26 +0100

    wrapper function for promise in console command

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ program
     .option('-o, --outname <name>', 'specify an output file name')
     .description('analyse json datas according to a given configuration')
     .action((...args) => {
-        analyseCommand(...args).catch(err => {
-            console.error(err)
-        })
+        callAsPromise(analyseCommand, args);
     });
 
     program
@@ -33,23 +31,25 @@ program
     .option('-d, --discardLower <discardLower>', 'All nodes with lower frequency than that won\'t be in final result', 10)
     .description('run a frenquency analysis to try to find the nodes wich are part of a path, and one which are ids, and determine json structure.')
     .action((...args) => {
-        frenquencyCommand(...args).catch(err => {
-            console.error(err)
-        })
+        callAsPromise(frenquencyCommand, args);
     });
 
 
     program
-    .command('create_config <pathToFrequenciesFile>')
+    .command('create_config [pathToFrequenciesFile]')
     .alias('cc')
     .option('-o, --outname <name>', 'specify an output file name')
     .description('Ask a suite of questions to help generate a config file from a frequecy analysis result file.')
     .action((...args) => {
-        confFromFrequencies(...args).catch(err => {
-            console.error(err)
-        })
+        callAsPromise(confFromFrequencies, args);
     });
 
+
+const callAsPromise = (toCall, args) => {
+    toCall(...args).then(()=>{process.exit()}).catch(err => {
+        console.error(err)
+    })
+}
 
 const analyseCommand = async (configPath, maxReject, maxExampleValues, cmd) => {
     let config = await prepareAnalysis(configPath, cmd.distant, cmd.path, cmd.url, maxReject, maxExampleValues);
@@ -59,7 +59,7 @@ const analyseCommand = async (configPath, maxReject, maxExampleValues, cmd) => {
 }
 
 const frenquencyCommand = async (path, cmd) => {
-    await fanalysis(path, cmd.outname, cmd.discardLower, cmd.outname)
+    await fanalysis(path, cmd.mode, cmd.discardLower, cmd.outname)
 }
 
 const confFromFrequencies = async (path, cmd) => {
